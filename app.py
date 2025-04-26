a3e072f17484a3fab5747bd2862954e164655588
NBX7777 <45907487+NBX7777@users.noreply.github.com>
2025-04-27 01:22:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
+@app.route("/", methods=["GET"])
+def home():
+    return "Welcome to BulletsFlyAPI!"
+
+
 @app.route("/meme", methods=["POST"])
 def get_meme():
     data = request.get_json()
     text = data.get("text")
 
-    # 假設固定回傳一張梗圖網址
     return jsonify({"text_received": text, "meme_url": "https://imgur.com/b6wGgOg.jpg"})
 
 

5894535c1b234bc8853b6602b0ba04503b954264
NBX7777 <45907487+NBX7777@users.noreply.github.com>
2025-04-27 13:49:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
-@app.route("/", methods=["GET"])
-def home():
-    return "Welcome to BulletsFlyAPI!"
+# 關鍵字 -> 對應多張圖片
+meme_map = {
+    "噁心": ["https://imgur.com/a/rA0M9y0"],
+    "翻譯翻譯": ["https://imgur.com/a/vREicB4"],
+    "公平": ["https://imgur.com/h4af9O4"],
+}
 
 
 @app.route("/meme", methods=["POST"])
@@ -14,9 +16,17 @@ def get_meme():
         data = request.get_json()
         text = data.get("text", "")
 
-        return jsonify(
-            {"text_received": text, "meme_url": "https://imgur.com/b6wGgOg.jpg"}
-        )
+        result_images = []
+
+        for keyword, img_list in meme_map.items():
+            if keyword in text:
+                result_images.extend(img_list)  # 把這個關鍵字對應的圖片都加進結果
+
+        if result_images:
+            return jsonify({"image_urls": result_images})
+        else:
+            return jsonify({"image_urls": ["https://imgur.com/aeFk9vN"]})
+
     else:
         return jsonify({"error": "Request must be JSON"}), 400
 
